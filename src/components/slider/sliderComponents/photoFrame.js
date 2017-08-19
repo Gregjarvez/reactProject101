4cb999e512eb7efb0fab24da160316db4f335b67
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-08-19 19:07:20 +0000

    slider fixed

--- src/components/slider/sliderComponents/photoFrame.js
+++ src/components/slider/sliderComponents/photoFrame.js
@@ -1,1 +1,16 @@
-import React from 'react';const PhotoFrame = props => {  const isNull = () => false;  return (    <img      className={props._classname}      src={props._currentPhotoUrl}      onClick={props._onClick || isNull}    />  )}export default PhotoFrame;
+import React from 'react';
+
+const PhotoFrame = props => {
+  const isNull = () => false;
+
+  return (
+    <img
+      className={props._classname}
+      src={props._currentPhotoUrl}
+      onClick={() => (props._onClick || isNull)}
+    />
+
+  )
+}
+
+export default PhotoFrame;
