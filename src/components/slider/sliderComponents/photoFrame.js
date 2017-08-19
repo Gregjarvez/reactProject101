7c0e4209f010024e4b340ccd2114eb906311f36f
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-08-19 18:55:43 +0100

    components refactor

---
+++ src/components/slider/sliderComponents/photoFrame.js
@@ -0,0 +1,1 @@
+import React from 'react';const PhotoFrame = props => {  const isNull = () => false;  return (    <img      className={props._classname}      src={props._currentPhotoUrl}      onClick={props._onClick || isNull}    />  )}export default PhotoFrame;
