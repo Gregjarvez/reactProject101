d6abed7e7dbb885778577c515f7279c4d8a8556f
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-08-20 02:56:32 +0100

    .gitignore file fixed

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -1,12 +1,1 @@
-export default (props) => {
-
-    return (
-        <div className="c-tooltip">
-            { props.open ? <div className="c-tooltip__content">{ props.children }</div> : <div></div> }
-            <button
-                className="c-tooltip__button"
-                onClick={props.onToggle}
-            >{ props.buttonText }</button>
-        </div>
-    );
-};
+export default (props) => {    return (        <div className="c-tooltip">            { props.open ? <div className="c-tooltip__content">{ props.children }</div> : <div></div> }            <button                className="c-tooltip__button"                onClick={props.onToggle}            >{ props.buttonText }</button>        </div>    );};
